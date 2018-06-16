11c48e3e83f5bde2d65831c0f8485bc3a59ecce4
Szsaunders <sadikisaunders@gmail.com>
2018-06-16 10:37:32 -0400

    New script code that accounts for begin date, end datea dn number of records to pull

--- nytmpz.js
+++ nytmpz.js
@@ -1,18 +1,47 @@
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-    'api-key': "c9abce99a40e43cba203321b6a5bd4fd",
-    'q': "great depression"
-});
-$.ajax({
-    url: url,
-    method: 'GET',
-}).done(function(result) {
-    for (var i = 0; i < result.response.docs.length; i++) {
-        $("#article-titles").append(result.response.docs[i].headline.main + "<br>");
-    }
+// var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+// url += '?' + $.param({
+//     'api-key': "c9abce99a40e43cba203321b6a5bd4fd",
+//     'q': "great depression"
+// });
+// $.ajax({
+//     url: url,
+//     method: 'GET',
+// }).done(function(result) {
+//     for (var i = 0; i < result.response.docs.length; i++) {
+//         $("#article-titles").append(result.response.docs[i].headline.main + "<br>");
+//     }
 
+// }).fail(function(err) {
+//     throw err;
+// });
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+var query = "placeholder";
+var numberRecords = 5;
+var beginDate = "00000000";
+var endDate = "00000000";
+url += '?' + $.param({
+'api-key': "b979efd373ab4ede84ab7ff3d79ac33d",
+'q': query,
+});
+if (beginDate != "00000000") {
+url += '&begin_date=' + beginDate;
+}
+if (endDate != "00000000") {
+url += '&end_date=' + endDate;
+}
+$.ajax({
+url: url,
+method: 'GET',
+}).done(function(result) {
+for (var i = 0; i < numberRecords; i++) {
+var articleDiv = $("<div>");
+var snippetP = $("<p>");
+$(snippetP).html(result.response.docs[i].snippet);
+$(articleDiv).prepend(snippetP);
+$("#article-names").prepend(articleDiv); }
 }).fail(function(err) {
-    throw err;
+throw err;
 });
 
 // $("#search-team").attr("value")
